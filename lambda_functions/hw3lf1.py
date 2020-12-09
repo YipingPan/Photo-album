1d63106d8b3237d909de862e6666b2a5a5f5e73b
Yiping Pan <55120352+YipingPan@users.noreply.github.com>
2020-12-08 23:18:40 -0500

    Update hw3lf1.py

--- lambda_functions/hw3lf1.py
+++ lambda_functions/hw3lf1.py
@@ -1,6 +1,7 @@
 def lambda_handler(event, context):
     print("LF1 run successfully")
     print("I've updated LF1")
+    print("I've updated LF1 again")
     return {
         'statusCode': 200,
     }
