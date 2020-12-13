d51a38f99cd022633f12d3df6ae55ab493477441
Yiping Pan <55120352+YipingPan@users.noreply.github.com>
2020-12-12 21:16:49 -0500

    Delete hw3lf1.py

--- lambda_functions/hw3lf1.py
+++
@@ -1,7 +0,0 @@
-def lambda_handler(event, context):
-    print("LF1 run successfully")
-    print("I've updated LF1")
-    print("I've updated LF1 again")
-    return {
-        'statusCode': 200,
-    }
